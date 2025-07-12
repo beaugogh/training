340db84ceedcd52d7d21b03382619d9fffce4168
bo <bo.gao111@gmail.com>
2025-07-12 21:06:39 +0800

    add notebooks folder

--- model.py
+++ model.py
@@ -1,26 +1,32 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, Qwen3ForCausalLM
 import torch
+from time import time
 
 
 class HuggingfaceModel:
     def __init__(self, model_name: str):
+        print(f"Start loading model {model_name}...")
+        start_time = time()
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name, torch_dtype="auto", device_map="auto"
         )
         # self.model.eval()
-        print(f"Model {model_name} loaded successfully. Device: {self.model.device}")
+        diff_time = time() - start_time
+        print(f"Model {model_name} is loaded successfully. Device: {self.model.device}")
+        print(f"Time: {int(diff_time)}s")
 
     def generate(
         self,
         prompt: str,
         system_prompt: str = None,
-        max_new_tokens: int = 32768,
+        max_new_tokens: int = 4096,
         temperature: float = 0.1,
         appy_chat_template: bool = True,
         enable_thinking: bool = True,
     ):
         with torch.no_grad():
+            start_time = time()
             input_text = prompt
             if appy_chat_template:
                 messages = (
@@ -43,4 +49,5 @@ class HuggingfaceModel:
             response = self.tokenizer.decode(
                 output_ids, skip_special_tokens=True
             ).strip("\n")
+            print(f"Time: {int(time() - start_time)}s")
             return response
